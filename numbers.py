21e92b260e1c37b82e4ce71a8f41d5cc169c4ac3
Vishesh <visheshsingh16@gmail.com>
2017-10-14 23:52:32 -0400

    Numbers in python

---
+++ numbers.py
@@ -0,0 +1,41 @@
+# NUMBERS
+
+print(type(5)) #<class 'int'>
+
+print(type(3.142)) #<class 'float'>
+
+print(5 + 5) #10
+
+print(10 - 7) #3
+
+print(30 * 8) #240
+
+print(40 / 5) #8.0
+
+print(40 // 5) #8
+
+print(5 ** 5) #3125
+
+print(5 % 2) #1
+
+#BIDMAS - Brackets Indices Div Mul Add Sub
+
+print(5 + 5 * 8) #45
+
+print((10 - 4) * 3) #18
+
+# VARIABLES
+
+age = 18
+print(age)
+
+age += 2
+print(age)
+
+wages = 10000
+bills = 2000
+rent = 3000
+food = 1000
+
+savings = wages - bills - rent - food
+print(savings)
