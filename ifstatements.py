e7094b7c4e7e7ca2cf4fe856c0756834f03e6c56
Vishesh <visheshsingh16@gmail.com>
2017-10-15 00:41:01 -0400

    conditional stmts

---
+++ ifstatements.py
@@ -0,0 +1,17 @@
+# IF STATEMENTS
+
+age = int(input('enter your age: '))
+
+if age < 10:
+    print('You are a kid!')
+elif age < 25:
+    print('Enjoy your life!')
+else:
+    print('Stay young at heart!')
+
+meat = input('Do you eat meat? (y/n): ')
+
+if meat == 'y':
+    print('Enjoy your delicious non-veg dish!')
+else:
+    print('Enjoy your veggie delight!')
