d8ae363b4e8895002e90789e44e1a365813bec03
Vishesh <visheshsingh16@gmail.com>
2017-10-15 00:22:07 -0400

    Standard Input example

---
+++ StandardInput.py
@@ -0,0 +1,12 @@
+# STANDARD INPUT
+
+name = input('Enter your name:')
+age = input('Enter your age:')
+
+print('Hello', name, 'you are', age, 'years old')
+
+#Area of circle
+
+radius = int(input("Enter the radius: "))
+area = 3.142 * radius ** 2
+print(area)
