70efd68fa78c8009e9176f20648bbf2f4a07480b
Vishesh <visheshsingh16@gmail.com>
2017-10-15 00:57:21 -0400

    playing with ranges

---
+++ ranges.py
@@ -0,0 +1,26 @@
+# RANGES
+
+for i in range(5):
+    print(i)
+
+print("-------")
+
+for i in range(3, 10):
+    print(i)
+
+print("-------")
+
+for i in range(2, 20, 2):
+    print(i)
+
+menu = ['pizza', 'burger', 'biryani', 'chicken', 'eggs']
+
+print("-------")
+
+for n in range(len(menu)):
+    print(n, menu[n])
+
+print("-------")
+
+for n in range(len(menu)-1, -1, -1):
+    print(n, menu[n])
